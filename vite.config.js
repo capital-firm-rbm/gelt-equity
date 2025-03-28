d76bcd40b9bccffb8e04ee42eb37cd372f6f2ba1
Rafibarides <rafibaridesmusic@gmail.com>
2025-03-28 18:17:06 -0400

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Replace with your repository name
+  base: '/capital-firm/', // Must match your repository name with slashes
 })
