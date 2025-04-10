baa3f1d95dcb257f97a81e66309019a294828dbe
Rafibarides <rafibaridesmusic@gmail.com>
2025-04-10 16:39:57 -0400

    preview

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import './assets/favicon.png'
+import './assets/load-link.png'
 
 // Determine the base URL
 const basename = '/'
